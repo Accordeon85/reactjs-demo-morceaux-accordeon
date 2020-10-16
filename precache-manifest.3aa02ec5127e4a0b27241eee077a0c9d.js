b190f18b171c55be15b6e1dce2195fb68e6f5654
Thibaut <thibaut.pasquier.89@gmail.com>
2020-10-16 14:39:52 +0200

    Updates

---
+++ precache-manifest.3aa02ec5127e4a0b27241eee077a0c9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12143c67b3f97e5336193e05800ba1a",
+    "url": "/reacts-demo-morceaux-accordeon/index.html"
+  },
+  {
+    "revision": "cbf978929a72bb75eb3e",
+    "url": "/reacts-demo-morceaux-accordeon/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "11489e2f640b71029433",
+    "url": "/reacts-demo-morceaux-accordeon/static/css/main.6b71dc0c.chunk.css"
+  },
+  {
+    "revision": "cbf978929a72bb75eb3e",
+    "url": "/reacts-demo-morceaux-accordeon/static/js/2.7e697f07.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reacts-demo-morceaux-accordeon/static/js/2.7e697f07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11489e2f640b71029433",
+    "url": "/reacts-demo-morceaux-accordeon/static/js/main.17a6e0de.chunk.js"
+  },
+  {
+    "revision": "05df1b6c432c91f2cac3",
+    "url": "/reacts-demo-morceaux-accordeon/static/js/runtime-main.d99344b6.js"
+  }
+]);
